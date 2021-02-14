eeadf45294b8e2738cfc6c113907d9c5503b8c22
TanishqPorwar <tanishqporwar22@gmail.com>
2021-02-15 01:31:03 +0530

    refactored the code & added bloc

--- lib/src/services/auth_services.dart
+++ lib/src/services/auth_services.dart
@@ -8,7 +8,7 @@ String url = "174.138.121.47:8080";
 Future<String> register(String registrationNumber, String password) async {
   String body =
       json.encode({"reg_no": registrationNumber, "password": password});
-  var res = await http.post(url,
+  var res = await http.post(url + "/register",
       headers: {"Content-Type": "application/json"}, body: body);
   if (res.statusCode != 200) {
     return "error";
